5e46e12578dda6b00cca2f2739f83bcb76592de3
GithubPassword@123 <parzam77@gmail.com>
2021-12-02 16:36:45 +0530

    Implementation of Memoization

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,4 +1,5 @@
 import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
 
 let lastId = 0;
 
@@ -24,6 +25,11 @@ const slice = createSlice({
 export const { bugAdded, bugResolved } = slice.actions;
 export default slice.reducer;
 
-export const getUnresolvedBugs = (state) => {
-  return state.entities.bugs.filter((bug) => !bug.resolved);
-};
+//Memoization
+//bugs => get unresolved bugs from catche
+
+export const getUnresolvedBugs = createSelector(
+  (state) => state.entities.bugs,
+  (state) => state.entities.projects,
+  (bugs, projects) => bugs.filter((bug) => !bug.resolved) // this logic only executes when there is change in bugs list else get result from state
+);
