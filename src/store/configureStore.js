f7627765c78ae46a7fd1c18601a0c18644d80f80
GithubPassword@123 <parzam77@gmail.com>
2021-12-02 18:46:39 +0530

    Configuring redux-toolkit Thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,10 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducers";
 import logger from "./middleware/logger";
 import func from "./middleware/func";
+import { getDefaultMiddleware } from "@reduxjs/toolkit";
 
 export default function () {
   return configureStore({
     reducer,
-    middleware: [logger("console"), func],
+    middleware: [...getDefaultMiddleware(), logger("console")],
   });
 }
