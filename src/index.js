42e5bcd783c2e0bb7207a8b48492287a732dbdff
GithubPassword@123 <parzam77@gmail.com>
2021-12-02 18:16:40 +0530

    Creation of Middleware

--- src/index.js
+++ src/index.js
@@ -15,18 +15,18 @@ let unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch(projectAdded({ name: "project-1" }));
-store.dispatch(projectAdded({ name: "project-2" }));
-unsubscribe();
-store.dispatch(bugAdded({ description: "Bug1" }));
-store.dispatch(bugAdded({ description: "Bug2" }));
-store.dispatch(bugResolved({ Id: 2 }));
+// store.dispatch(projectAdded({ name: "project-2" }));
+// unsubscribe();
+// store.dispatch(bugAdded({ description: "Bug1" }));
+// store.dispatch(bugAdded({ description: "Bug2" }));
+// store.dispatch(bugResolved({ Id: 2 }));
 
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(bugsAssignedtoUser({ bugId: 1, userId: 1 }));
+// store.dispatch(userAdded({ name: "User 1" }));
+// store.dispatch(bugsAssignedtoUser({ bugId: 1, userId: 1 }));
 
-let unsolved = getUnresolvedBugs(store.getState());
-let bugsByuser = getBugsByUser(1)(store.getState());
-console.log(unsolved);
-console.log(bugsByuser);
+// let unsolved = getUnresolvedBugs(store.getState());
+// let bugsByuser = getBugsByUser(1)(store.getState());
+// console.log(unsolved);
+// console.log(bugsByuser);
 
-console.log(store.getState());
+// console.log(store.getState());
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducers";
+import logger from "./middleware/logger";
 
 export default function () {
   return configureStore({
     reducer,
+    middleware: [logger],
   });
 }
